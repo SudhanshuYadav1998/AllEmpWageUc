4d2cf79ddb56b42c499eaa1321a49d55dbdf3220
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-18 22:13:23 +0530

    Added UC-1 File

---
+++ UC-1/UC-1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("welcome to Employee Management app");
+int IsFullTime = 1;
+Random random = new Random();
+int EmpCheck = random.Next(0, 2);
+if (EmpCheck == IsFullTime)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
