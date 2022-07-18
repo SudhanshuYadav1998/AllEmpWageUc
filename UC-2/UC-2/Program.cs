470a2eafbe344395e147634c8b59d2ef58dfb62b
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-18 22:15:27 +0530

    Added UC-2 File

---
+++ UC-2/UC-2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("welcome to Employee Management app");
+int IsFullTime = 1;
+int empHrs = 0;
+int EmpWage = 0;
+Random random = new Random();
+int EmpWagePerHrs = 20;
+int EmpCheck = random.Next(0, 2);
+if (EmpCheck == IsFullTime)
+{
+    empHrs = 8;
+}
+else
+{
+    empHrs = 0;
+}
+EmpWage = empHrs * EmpWagePerHrs;
+Console.WriteLine("Employees wage is: " + EmpWage);
