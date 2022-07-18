4f303e066f24e3effa0c0f6d8971a6a5751690e8
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-18 22:17:01 +0530

    Added UC-3 File

---
+++ UC-3/UC-3/Program.cs
@@ -0,0 +1,22 @@
+﻿int IsFullTime = 1;
+int IsPartTime = 2;
+int empHrs = 0;
+int EmpWage = 0;
+Random random = new Random();
+int EmpWagePerHrs = 20;
+int EmpCheck = random.Next(0, 3);
+if (EmpCheck == IsFullTime)
+{
+    empHrs = 8;
+}
+else if (EmpCheck == IsPartTime)
+{
+
+    empHrs = 4;
+}
+else
+{
+    empHrs = 0;
+}
+EmpWage = empHrs * EmpWagePerHrs;
+Console.WriteLine("Employees wage is: " + EmpWage);
